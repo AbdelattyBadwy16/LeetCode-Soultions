42fe7eda958c29a5ce25edbe4034a542f65d6128
Abdelatty <abdelattybadwy166@gmail.com>
2024-08-12 13:02:12 +0300

    703-Problem

---
+++ 703. Kth Largest Element in a Stream.cpp
@@ -0,0 +1,23 @@
+class KthLargest {
+    int k;
+    priority_queue<int,vector<int>,greater<int>> minHeap;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(auto n : nums){
+            add(n);
+        }
+    }
+
+    int add(int val) {
+        minHeap.push(val);
+        if(minHeap.size()>k)minHeap.pop();
+        return minHeap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
