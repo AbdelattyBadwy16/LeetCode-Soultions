ccdf53ab21704e57d93e48a9ed2ba1f356aaedd0
AbdelattyBadwy16 <abdelattybadwy166@gmail.com>
2024-09-18 11:52:29 +0300

    179-Problem

--- 179. Largest Number.cpp
+++ 179. Largest Number.cpp
@@ -1,60 +1,31 @@
 class Solution {
 public:
-    void fill(vector<vector<int>>& v,vector<vector<int>> x,int index){
-        for(int k=0;k<3;k++){
-            v[index].push_back(x[0][k]);
-            v[index+1].push_back(x[1][k]);
-            v[index+2].push_back(x[2][k]);
-        }
+    static bool cmp(string a,string b){
+        string temp = a+b;
+        string temp2 = b+a;
+        return temp<temp2;
     }
-    void dfs(vector<vector<int>>&v , int i,int j){
-        vector<int> x={1,0,-1,0};
-        vector<int> y={0,1,0,-1};
-        v[i][j]=1;
-        for(int k=0;k<4;k++){
-            int i1=i+x[k];
-            int j1=j+y[k];
-            if(i1>=0 && j1>=0 && i1<v.size() && j1<v[0].size() && v[i1][j1]==0){
-                dfs(v,i1,j1);
+    string largestNumber(vector<int>& nums) {
+        vector<string>v;
+        for(auto it:nums){
+            string s = "";
+            if(!it){
+                v.push_back("0");
+                continue;
             }
-        }
-    }
-    int regionsBySlashes(vector<string>& grid) {
-        for(auto it:grid){
-            cout<<it<<endl;
-        }
-        int n=grid.size();
-        vector<vector<int>> v(3*n);
-        vector<vector<int>> e={{0,0,0},{0,0,0},{0,0,0}};
-        vector<vector<int>> a={{1,0,0},{0,1,0},{0,0,1}};
-        vector<vector<int>> b={{0,0,1},{0,1,0},{1,0,0}};
-        int index=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<grid[i].size();j++){
-                // cout<<grid[i][j];
-                if(grid[i][j]==' ') fill(v,e,index);
-                else if(grid[i][j]=='/') fill(v,b,index);
-                else{
-                    fill(v,a,index);
-                }
+            while(it){
+                s += (char)(it%10+'0');
+                it/=10;
             }
-            index+=3;
+            reverse(s.begin(),s.end());
+            v.push_back(s);
+        }        
+        sort(v.begin(),v.end(),cmp);
+        string s = "";
+        for(int i=v.size()-1 ; i>=0 ; i--){
+            s += v[i];
         }
-        for(int i=0;i<v.size();i++){
-            for(int j=0;j<v[0].size();j++){
-                cout<<v[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        int ans=0;
-        for(int i=0;i<v.size();i++){
-            for(int j=0;j<v[0].size();j++){
-                if(v[i][j]==0){
-                    ans++;
-                    dfs(v,i,j);
-                }
-            }
-        }
-        return ans;
+        if(s[0]=='0')return "0";
+        return s;
     }
 };
